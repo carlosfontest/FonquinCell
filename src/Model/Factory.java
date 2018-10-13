41abf0a26c79dcc9da5f33737f2b4470e113bba5
Rafael Quintero <rafaelq.sistemas@gmail.com>
2018-10-13 13:47:35 -0400

    continuation of the factory constructor and some new methods

--- src/Model/Factory.java
+++ src/Model/Factory.java
@@ -18,17 +18,62 @@ public class Factory {
     // Assembler's array
     private Assembler[] assemblers;
     // Semaphores
-    private Semaphore prodS, prodB, prodC, mutexS, mutexB, mutexC, mutexA, assS, assB, assC, timerManager;
+    private Semaphore prodS, prodB, prodC, mutexPS, mutexPB, mutexPC, mutexAS, mutexAB, mutexAC, mutexA, assS, assB, assC, timerManager;
     // Buffer pointers (Critical section)
     private int nextPosPS, nextPosPB, nextPosPC, nextPosAS, nextPosAB, nextPosAC;
 
     public Factory(int dayTime, int daysForDelivery, int screensStorageMax, int batteriesStorageMax, int cablesStorageMax, int batteriesInitProd, int screensInitProd, int cablesInitProd, int cablesMaxProd, int screensMaxProd, int batteriesMaxProd, int initAss, int maxAss) {
+        // Initializing production semaphores
+        this.prodS = new Semaphore(screensInitProd);
+        this.prodB = new Semaphore(batteriesInitProd);
+        this.prodC = new Semaphore(cablesInitProd);
+        // Initializing mutex semaphores
+        this.mutexPS = new Semaphore(1);
+        this.mutexPC = new Semaphore(1);
+        this.mutexPB = new Semaphore(1);
+        this.mutexAS = new Semaphore(1);
+        this.mutexAB = new Semaphore(1);
+        this.mutexAC = new Semaphore(1);
+        this.mutexA = new Semaphore(1);
+        // Initializing assembler semaphores
+        this.assS = new Semaphore(0);
+        this.assB = new Semaphore(0);
+        this.assC = new Semaphore(0);
+        // Initializing next positions
+        this.nextPosPS = 0;
+        this.nextPosPB = 0;
+        this.nextPosPC = 0;
+        this.nextPosAS = 0;
+        this.nextPosAC = 0;
+        this.nextPosAB = 0;
+        // Initializing counters
+        this.assemblerCount = 0;
+        this.batteriesCount = 0;
+        this.screensCount = 0;
+        this.cablesCount = 0;
+        // Assigning JSON Values
         this.dayTime = dayTime;
         this.daysForDelivery = daysForDelivery;
         this.screens = new Storage(screensStorageMax);
         this.batteries = new Storage(batteriesStorageMax);
         this.cables = new Storage(cablesStorageMax);
-        // Seguir el config json para llenar este constructor
+        this.batteriesProd = new Producer[batteriesMaxProd];
+        this.screensProd = new Producer[screensMaxProd];
+        this.cablesProd = new Producer[cablesMaxProd];
+        // Initializing producers
+        for (int i = 0; i < batteriesInitProd; i++) {
+            this.batteriesProd[i] = this.newBatteriesProd();
+        }
+        for (int i = 0; i < screensInitProd; i++) {
+            this.screensProd[i] = this.newScreensProd();
+        }
+        for (int i = 0; i < cablesInitProd; i++) {
+            this.cablesProd[i] = this.newCablesProd();
+        }
+        // Initializing assemblers
+        for (int i = 0; i < initAss; i++) {
+            this.assemblers[i] = this.newAssembler();
+        }
     }
     
     
@@ -39,5 +84,22 @@ public class Factory {
 
     public int getDayTime() {
         return dayTime;
+    }
+    
+    public Producer newBatteriesProd(){
+        this.batteriesCount++;
+        return new Producer(this.batteries,this.mutexPB,this.prodB, this.assB, this.dayTime, this.nextPosPB);
+    }
+    public Producer newScreensProd(){
+        this.screensCount++;
+        return new Producer(this.screens,this.mutexPS,this.prodS, this.assS, this.dayTime*2, this.nextPosPS);
+    }
+    public Producer newCablesProd(){
+        this.cablesCount++;
+        return new Producer(this.cables,this.mutexPC,this.prodC, this.assC, this.dayTime, this.nextPosPC);
+    }
+    public Assembler newAssembler(){
+        this.assemblerCount++;
+        return nw
     }
 }
